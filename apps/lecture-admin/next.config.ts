e39f878b40f4f79589586acf3dc46d0a3bce373b
MrJeleika <den932005@gmail.com>
2026-01-25 11:11:41 +0100

    lecture admin build

--- apps/lecture-admin/next.config.ts
+++ apps/lecture-admin/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  /* config options here */
 };
 
 export default nextConfig;
+
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
