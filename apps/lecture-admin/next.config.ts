ccf2041a5b07a4f9a178a16e8ab3fc5ee4670995
MrJeleika <den932005@gmail.com>
2026-01-25 13:04:03 +0100

    build lecture admin

--- apps/lecture-admin/next.config.ts
+++ apps/lecture-admin/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 import path from 'path';
 
 const nextConfig: NextConfig = {
-  turbopack:{
+  outputFileTracingRoot: path.join(__dirname, '../..'),
+  turbopack: {
     root: path.join(__dirname, '../..'),
   }
 };
