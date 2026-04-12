bc052dd558c9256588c914da52b517a70cc601f7
MrJeleika <den932005@gmail.com>
2026-04-12 20:54:07 +0200

    fix: db ssl

--- apps/backend/src/db/index.ts
+++ apps/backend/src/db/index.ts
@@ -6,5 +6,7 @@ import { payloadSchema } from '@redduck/payload-config'
 
 const client = postgres(env.DATABASE_URL)
 
-export const db = drizzle(client, { schema })
-export const payloadDb = drizzle(client, { schema: payloadSchema })
+// @ts-expect-error - connection is not supported in drizzle-orm
+export const db = drizzle(client, { schema, connection: { ssl: { rejectUnauthorized: false } } })
+// @ts-expect-error - connection is not supported in drizzle-orm
+export const payloadDb = drizzle(client, { schema: payloadSchema, connection: { ssl: { rejectUnauthorized: false } } })
