08e8aed47ac978c024c2b2f73e987ae7a0f3001c
MrJeleika <den932005@gmail.com>
2026-04-12 22:37:56 +0200

    fix: origin

--- apps/backend/src/lib/auth.ts
+++ apps/backend/src/lib/auth.ts
@@ -9,7 +9,7 @@ import { db } from '../db'
 export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: 'pg', schema }),
   appName: 'Redduck Academy',
-  trustedOrigins: ['http://localhost:3000', 'http://localhost:3001'],
+  trustedOrigins: ['http://localhost:3000', 'http://localhost:3001', 'https://redduck-academy.jeleika.com'],
   baseURL: env.BETTER_AUTH_URL,
   secret: env.BETTER_AUTH_SECRET,
   socialProviders: {
