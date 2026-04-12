0807b26dad218f38ce1176b9c59f055316f1b057
MrJeleika <den932005@gmail.com>
2026-04-12 22:51:16 +0200

    fix: update database connection to use SSL configuration

--- apps/backend/src/db/index.ts
+++ apps/backend/src/db/index.ts
@@ -4,9 +4,7 @@ import { env } from '../env'
 import * as schema from './schema'
 import { payloadSchema } from '@redduck/payload-config'
 
-const client = postgres(env.DATABASE_URL)
+const client = postgres(env.DATABASE_URL, { ssl: { rejectUnauthorized: false } })
 
-// @ts-expect-error - connection is not supported in drizzle-orm
-export const db = drizzle(client, { schema, connection: { ssl: { rejectUnauthorized: false } } })
-// @ts-expect-error - connection is not supported in drizzle-orm
-export const payloadDb = drizzle(client, { schema: payloadSchema, connection: { ssl: { rejectUnauthorized: false } } })
+export const db = drizzle(client, { schema })
+export const payloadDb = drizzle(client, { schema: payloadSchema })
