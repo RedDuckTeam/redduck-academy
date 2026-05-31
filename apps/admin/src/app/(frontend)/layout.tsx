88bab0a3e53d23702dab1b75fdc9fe7ac043bcbe
MrJeleika <den932005@gmail.com>
2026-06-01 01:49:40 +0200

    fix: rebuild admin

--- apps/admin/src/app/(frontend)/layout.tsx
+++ apps/admin/src/app/(frontend)/layout.tsx
@@ -3,7 +3,7 @@ import './styles.css'
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Templat',
+  title: 'Payload Blank Template',
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
