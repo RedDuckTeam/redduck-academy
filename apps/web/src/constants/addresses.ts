5573ad486a57eb4f70cc084ea1c968639e4b7acb
MrJeleika <den932005@gmail.com>
2026-07-11 20:11:05 +0200

    feat: mainnet contract address

--- apps/web/src/constants/addresses.ts
+++ apps/web/src/constants/addresses.ts
@@ -1,4 +1,4 @@
 export const certificateContractAddresses = {
   sepolia: '0x709a09F74aA21E00F117DF067BF6cf96D8D34B11' as `0x${string}`,
-  mainnet: '' as `0x${string}`,
+  mainnet: '0x3E57c8DfF046fDB44D61cF661Ba3fc87078dEF27' as `0x${string}`,
 }
